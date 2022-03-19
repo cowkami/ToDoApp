43189b95aa6fcff7ce9c13320b8afc51a24c422d
lee unseo <lee.unseo17@gmail.com>
2022-03-20 01:14:48 +0900

    Modify App.vue to show todo list.

--- frontend/todo/src/App.vue
+++ frontend/todo/src/App.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
 import { reactive } from "vue";
-const state = reactive({ count: 0 });
-const increment = () => {
-  state.count++;
+
+interface ToDo {
+  description: string;
+  done: boolean;
+}
+
+interface ToDoList {
+  list: ToDo[];
+}
+
+const todoModel = reactive<ToDoList>({ list: [] });
+
+const newToDo = () => {
+  const todo: ToDo = {
+    description: "new todo",
+    done: false,
+  };
+  todoModel.list.push(todo);
 };
 </script>
 
 <template>
   <v-app>
-    <v-main> hello </v-main>
+    <v-app-bar color="prymary">
+      <v-toolbar-title>ToDo</v-toolbar-title>
+      <v-btn text elevation="3" @click="newToDo">new</v-btn>
+    </v-app-bar>
+    <v-main>
+      <ul>
+        <li v-for="todo in todoModel.list">{{ todo.description }}</li>
+      </ul>
+    </v-main>
   </v-app>
 </template>
