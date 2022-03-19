10546450d9e196b1f1495ca3697631038a6644ff
lee unseo <lee.unseo17@gmail.com>
2022-03-19 23:25:34 +0900

    Format index.html and modify App.vue

--- frontend/todo/src/App.vue
+++ frontend/todo/src/App.vue
@@ -1,26 +1,13 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+const state = reactive({ count: 0 });
+const increment = () => {
+  state.count++;
+};
+</script>
+
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-main> hello </v-main>
   </v-app>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data () {
-    return {
-      //
-    }
-  },
-})
-</script>
