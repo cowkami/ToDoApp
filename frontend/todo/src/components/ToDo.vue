3bea5183354c8d24f21dcc944c4bbee977e9e335
lee unseo <lee.unseo17@gmail.com>
2022-04-03 21:40:00 +0900

    Tune layouts

--- frontend/todo/src/components/ToDo.vue
+++ frontend/todo/src/components/ToDo.vue
@@ -8,22 +8,26 @@ defineProps<{
 </script>
 
 <template>
-  <v-container elevation="10">
-    <v-row>
-      <v-btn @click="changeDone(index.toString(), todo)" elevation="0" icon>
-        <v-icon v-if="todo.done">mdi-check-circle</v-icon>
-        <v-icon v-else>mdi-checkbox-blank-circle</v-icon>
-      </v-btn>
-
-      <v-textarea
-        v-model="todo.description"
-        v-bind:id="index.toString()"
-        auto-grow
-        rows="1"
-        v-bind:hide-details="true"
-        density="compact"
-      >
-      </v-textarea>
+  <v-container elevation="10" class="pa-1" fluid>
+    <v-row class="pa-1">
+      <v-col cols="auto" class="pa-1">
+        <v-container class="pa-1">
+          <v-btn @click="changeDone(index.toString(), todo)" elevation="0" icon>
+            <v-icon v-if="todo.done">mdi-check-circle</v-icon>
+            <v-icon v-else>mdi-checkbox-blank-circle</v-icon>
+          </v-btn>
+        </v-container>
+      </v-col>
+      <v-col class="pa-1">
+        <v-container fluid class="pl-0">
+          <textarea
+            rows="1"
+            v-model="todo.description"
+            v-bind:id="index.toString()"
+            style="color: white; width: 100%"
+          />
+        </v-container>
+      </v-col>
     </v-row>
   </v-container>
 </template>
